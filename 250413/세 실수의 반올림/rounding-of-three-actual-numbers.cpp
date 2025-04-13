dd5b2fd5db09a29537e775537c47be9c54723d8d
xolnvuri <12elizabet@naver.com>
2025-04-13 23:08:22 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250413/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout.setf(ios::fixed);
+    cout.precision(3);
+    cout << a << endl << b << endl << c << endl;
+    return 0;
+}
