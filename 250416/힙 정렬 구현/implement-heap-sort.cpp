e5b0bb142a40db8ba16ae27ff6193737aec0c997
xolnvuri <12elizabet@naver.com>
2025-04-16 23:03:54 +0900

    Title: 힙 정렬 구현, Time: 38ms, Memory: 1MB, Status: Passed - Codetree

--- 250416/힙 정렬 구현/implement-heap-sort.cpp
+++ 250416/힙 정렬 구현/implement-heap-sort.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // 최대 힙 구성
-void heapify(int arr[], int n, int i) {
+void heapify(vector<int>& arr, int n, int i) {
     int largest = i;
     int left = 2*i + 1;
     int right = 2*i + 2;
@@ -19,15 +20,17 @@ void heapify(int arr[], int n, int i) {
     }
 }
 
-void heapSort(int arr[], int n) {
+void heapSort(vector<int>& arr) {
+    int n = arr.size();
+
     // 최대 힙 만들기
     for (int i = n/2 - 1; i >= 0; i--)
         heapify(arr, n, i);
 
-    // 하나씩 꺼내면서 정렬
+    // 힙에서 하나씩 꺼내 정렬
     for (int i = n - 1; i > 0; i--) {
         swap(arr[0], arr[i]); // 가장 큰 걸 맨 뒤로
-        heapify(arr, i, 0);    // 남은 부분 힙 정렬
+        heapify(arr, i, 0);    // 남은 부분 다시 힙 정렬
     }
 }
 
@@ -35,11 +38,11 @@ int main() {
     int n;
     cin >> n;
 
-    int arr[1000]; // 입력이 1000개 이하일 경우
+    vector<int> arr(n);
     for (int i = 0; i < n; ++i)
         cin >> arr[i];
 
-    heapSort(arr, n);
+    heapSort(arr);
 
     for (int i = 0; i < n; ++i)
         cout << arr[i] << " ";
