b1bc61c853377ba5040688e1d40a13a628ea5805
xolnvuri <12elizabet@naver.com>
2025-05-25 17:48:19 +0900

    Title: 정수 명령 처리, Time: 14ms, Memory: 1MB, Status: Passed - Codetree

--- 250525/정수 명령 처리/process-numeric-commands.cpp
+++ 250525/정수 명령 처리/process-numeric-commands.cpp
@@ -1,13 +1,33 @@
+#include <iostream>
+#include <stack>
+using namespace std;
 
+int N;
+string command[10000];
+int value[10000];
 
-                                                                                
-                                                                                
-                                                                                                                        
-                                                                                                                
-                                                                                                                                                            
-                                                                                                                                                                        
-                                                                                                                                
-                                                                                                                                                                        
+int main() {
+    cin >> N;
+        for (int i = 0; i < N; i++) {
+                cin >> command[i];
+                        if (command[i] == "push") {
+                                    cin >> value[i];
+                                            }
+                                                }
+
+                                                    stack<int> s;
+                                                        for (int i = 0; i < N; i++) {
+                                                                if (command[i] == "push") {
+                                                                            s.push(value[i]);
+                                                                                    } else if (command[i] == "pop") {
+                                                                                                if (s.empty()) {
+                                                                                                                cout << -1 << endl;
+                                                                                                                            } else {
+                                                                                                                                            cout << s.top() << endl;
+                                                                                                                                                            s.pop();
+                                                                                                                                                                        }
+                                                                                                                                                                                } else if (command[i] == "size") {
+                                                                                                                                                                                            cout << s.size() << endl;
                                                                                                                                                                                                     } else if (command[i] == "empty") {
                                                                                                                                                                                                                 cout << (s.empty() ? 1 : 0) << endl;
                                                                                                                                                                                                                         } else if (command[i] == "top") {
@@ -20,28 +40,16 @@
                                                                                                                                                                                                                                                                                                         }
                                                                                                                                                                                                                                                                                                             return 0;
                                                                                                                                                                                                                                                                                                             }
-                                                                                                                                                                                                                                                                                                            #include <iostream>
-                                                                                                                                                                                                                                                                                                            #include <stack>
-                                                                                                                                                                                                                                                                                                            using namespace std;
+                                                                                                                                                                                                                                                                                                            
 
-                                                                                                                                                                                                                                                                                                            int N;
-                                                                                                                                                                                                                                                                                                            string command[10000];
-                                                                                                                                                                                                                                                                                                            int value[10000];
-
-                                                                                                                                                                                                                                                                                                            int main() {
-                                                                                                                                                                                                                                                                                                                cin >> N;
-                                                                                                                                                                                                                                                                                                                    for (int i = 0; i < N; i++) {
-                                                                                                                                                                                                                                                                                                                            cin >> command[i];
-                                                                                                                                                                                                                                                                                                                                    if (command[i] == "push") {
-                                                                                                                                                                                                                                                                                                                                                cin >> value[i];
-                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                            }
-
-                                                                                                                                                                                                                                                                                                                                                                stack<int> s;
-                                                                                                                                                                                                                                                                                                                                                                    for (int i = 0; i < N; i++) {
-                                                                                                                                                                                                                                                                                                                                                                            if (command[i] == "push") {
-                                                                                                                                                                                                                                                                                                                                                                                        s.push(value[i]);
-                                                                                                                                                                                        
+                                                                                
+                                                                                
+                                                                                                                        
+                                                                                                                
+                                                                                                                                                            
+                                                                                                                                                                        
+                                                                                                                                
+                                                                                                                                                                        
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
                                                                         
 
