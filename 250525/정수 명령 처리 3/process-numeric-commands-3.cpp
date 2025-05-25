f464fe8630b5ac4d3a9659bc60f3ccc096ff1b95
xolnvuri <12elizabet@naver.com>
2025-05-25 18:09:08 +0900

    Title: 정수 명령 처리 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250525/정수 명령 처리 3/process-numeric-commands-3.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    int N;
+        cin >> N;
+
+            string cmd;
+                int x;
+                    deque<int> dq;
+
+                        for (int i = 0; i < N; i++) {
+                                cin >> cmd;
+                                        
+                                                if (cmd == "push_front") {
+                                                            cin >> x;
+                                                                        dq.push_front(x);
+                                                                                }
+                                                                                        else if (cmd == "push_back") {
+                                                                                                    cin >> x;
+                                                                                                                dq.push_back(x);
+                                                                                                                        }
+                                                                                                                                else if (cmd == "pop_front") {
+                                                                                                                                            if (dq.empty()) cout << -1 << '\n';
+                                                                                                                                                        else {
+                                                                                                                                                                        cout << dq.front() << '\n';
+                                                                                                                                                                                        dq.pop_front();
+                                                                                                                                                                                                    }
+                                                                                                                                                                                                            }
+                                                                                                                                                                                                                    else if (cmd == "pop_back") {
+                                                                                                                                                                                                                                if (dq.empty()) cout << -1 << '\n';
+                                                                                                                                                                                                                                            else {
+                                                                                                                                                                                                                                                            cout << dq.back() << '\n';
+                                                                                                                                                                                                                                                                            dq.pop_back();
+                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                        else if (cmd == "size") {
+                                                                                                                                                                                                                                                                                                                    cout << dq.size() << '\n';
+                                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                                                    else if (cmd == "empty") {
+                                                                                                                                                                                                                                                                                                                                                cout << (dq.empty() ? 1 : 0) << '\n';
+                                                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                                                else if (cmd == "front") {
+                                                                                                                                                                                                                                                                                                                                                                            if (dq.empty()) cout << -1 << '\n';
+                                                                                                                                                                                                                                                                                                                                                                                        else cout << dq.front() << '\n';
+                                                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                                                        else if (cmd == "back") {
+                                                                                                                                                                                                                                                                                                                                                                                                                    if (dq.empty()) cout << -1 << '\n';
+                                                                                                                                                                                                                                                                                                                                                                                                                                else cout << dq.back() << '\n';
+                                                                                                                                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                return 0;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                
