f13079430b0f640afd3db1d67f11bfb950b3d23e
xolnvuri <12elizabet@naver.com>
2025-05-25 17:58:37 +0900

    Title: 정수 명령 처리 2, Time: 17ms, Memory: 1MB, Status: Passed - Codetree

--- 250525/정수 명령 처리 2/process-numeric-commands-2.cpp
+++ 250525/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
-#include <queue>
 #include <string>
+#include <queue>
+
 using namespace std;
 
+int N;
+string command[10000];
+int A[10000];
+
 int main() {
-    int N;
-        cin >> N;
-            
-                queue<int> q;
-                    string command;
-                        int value;
+    cin >> N;
 
-                            for (int i = 0; i < N; i++) {
-                                    cin >> command;
-                                            if (command == "push") {
-                                                        cin >> value;
-                                                                    q.push(value);
-                                                                            } else if (command == "pop") {
-                                                                                        if (!q.empty()) q.pop();
-                                                                                                } else if (command == "front") {
-                                                                                                            if (!q.empty()) cout << q.front() << endl;
-                                                                                                                    } else if (command == "size") {
-                                                                                                                                cout << q.size() << endl;
-                                                                                                                                        } else if (command == "empty") {
-                                                                                                                                                    cout << (q.empty() ? 1 : 0) << endl;
-                                                                                                                                                            }
-                                                                                                                                                                }
+        for (int i = 0; i < N; i++) {
+                cin >> command[i];
+                        if (command[i] == "push") {
+                                    cin >> A[i];
+                                            }
+                                                }
 
-                                                                                                                                                                    return 0;
-                                                                                                                                                                    }
-                                                                                                                                                                    
+                                                    queue<int> q;
+
+                                                        for (int i = 0; i < N; i++) {
+                                                                if (command[i] == "push") {
+                                                                            q.push(A[i]);
+                                                                                    } else if (command[i] == "pop") {
+                                                                                                if (!q.empty()) {
+                                                                                                                cout << q.front() << endl;
+                                                                                                                                q.pop();
+                                                                                                                                            } else {
+                                                                                                                                                            cout << 0 << endl;  // 비어있으면 0 출력
+                                                                                                                                                                        }
+                                                                                                                                                                                } else if (command[i] == "front") {
+                                                                                                                                                                                            if (!q.empty()) {
+                                                                                                                                                                                                            cout << q.front() << endl;
+                                                                                                                                                                                                                        } else {
+                                                                                                                                                                                                                                        cout << 0 << endl;  // 비어있으면 0 출력
+                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                            } else if (command[i] == "size") {
+                                                                                                                                                                                                                                                                        cout << q.size() << endl;
+                                                                                                                                                                                                                                                                                } else if (command[i] == "empty") {
+                                                                                                                                                                                                                                                                                            cout << (q.empty() ? 1 : 0) << endl;
+                                                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                                                        }
+
+                                                                                                                                                                                                                                                                                                            return 0;
+                                                                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                                                                            
